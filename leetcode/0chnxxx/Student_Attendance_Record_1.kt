dd65462a0dff1658d90a3fd3f6d7bab241be8b00
chan <0chn.xxx@gmail.com>
2026-02-24 10:10:50 +0900

    [LTC] Lv1 / Student Attendance Record 1 / 3분

---
+++ leetcode/0chnxxx/Student_Attendance_Record_1.kt
@@ -0,0 +1,52 @@
+/**
+ * You are given a string s representing an attendance record for a student where each character signifies whether the student was absent, late, or present on that day.
+ * The record only contains the following three characters:
+ * 'A': Absent.
+ * 'L': Late.
+ * 'P': Present.
+ *
+ * The student is eligible for an attendance award if they meet both of the following criteria:
+ * The student was absent ('A') for strictly fewer than 2 days total.
+ * The student was never late ('L') for 3 or more consecutive days.
+ * Return true if the student is eligible for an attendance award, or false otherwise.
+ *
+ * Constraints:
+ * 1 <= s.length <= 1000
+ * s[i] is either 'A', 'L', or 'P'.
+ */
+
+fun main() {
+    val s = "PPALLP"
+
+    val result = Solution().checkRecord(s)
+
+    println(result)
+}
+
+class Solution {
+    // 시간 복잡도 : O(N)
+    // 공간 복잡도 : O(1)
+    fun checkRecord(s: String): Boolean {
+        var absentCount = 0
+        var lateStreak = 0
+
+        for (c in s) {
+            when (c) {
+                'A' -> {
+                    ++absentCount
+                    if (absentCount >= 2) return false
+                    lateStreak = 0
+                }
+                'L' -> {
+                    lateStreak++
+                    if (lateStreak >= 3) return false
+                }
+                'P' -> {
+                    lateStreak = 0
+                }
+            }
+        }
+
+        return true
+    }
+}
